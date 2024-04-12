5ada20c525f1d726136a501c2047eaa347b103f6
= <exg1108@mavs.uta.edu>
2024-04-12 12:13:00 -0500

    login php file created

--- PHP/connect_to_database.php
+++ PHP/connect_to_database.php
@@ -1,16 +1,6 @@
 <?php   // First line of PHP code written at 10:53 A.M. on 4/12/2024
         // File created on 10:50 A.M. on 4/12/2024
         // Database created on 11:09 A.M. on 4/12/2024
-        
-        
-        
-        // if (isset($_POST["submit"])) {
-        //         echo "Registration successful!";
-        // }
-        // else {
-        //         header("location: HTML/signup.html");
-        // }
-        
 
         $servername = "localhost";
         $database_username = "root";
--- PHP/registration.php
+++ PHP/registration.php
@@ -1,5 +1,13 @@
 <?php
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
+    if (isset($_POST["submit"])) {
+        
+        // $username = $_POST['username'];
+        // $password = $_POST['password'];
+        // $email = $_POST['email'];
+    
+        echo "Registration successful!";
+    }
+    else {
+        header("location : HTML\signup.html");
+    }
