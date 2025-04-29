f81676b42126e5402706fb5fcc1ffc44170b613a
= <ericanthonygurley@gmail.com>
2025-04-28 20:24:59 -0500

    skill tree implemented

--- index.php
+++ index.php
@@ -15,34 +15,59 @@
         <h1> Owlgorithms </h1>
     </div>
 
-    <div class = "tree">
-        <button type = "button" class = "tree_button" id = "algorithms">
-            ??????????
-        </button>
+    <!--
+    Level 4
+    -->
 
-        <button type = "button" class = "tree_button" id = "struct_pointers">
-            Struct Pointers
-        </button>
+    <div class = "level" id = "four">
 
-        <button type = "button" class = "tree_button" id = "structs">
-            Structs
-        </button>
+    <button type = "button" class = "tree_button" id = "struct_pointers">
+        Struct Pointers
+    </button>
 
-        <button type = "button" class = "tree_button" id = "pointers">
-            Pointers
-        </button>
+    <button type = "button" class = "tree_button" id = "structs">
+        Structs
+    </button>
 
-        <button type = "button" class = "tree_button" id = "functions">
-            Functions
-        </button>
+    <button type = "button" class = "tree_button" id = "pointers">
+        Pointers
+    </button>
+
+    </div>
+
+    <!--
+    Level 3
+    -->
+
+    <div class = "level" id = "three">
+
+    <button type = "button" class = "tree_button" id = "functions">
+        Functions
+    </button>
+
+    </div>
+
+    <!--
+    Level 2
+    -->
+
+    <div class = "level" id = "two">
 
         <button type = "button" class = "tree_button" id = "for">
             For
         </button>
-    
+
         <button type = "button" class = "tree_button" id = "while">
             While
         </button>
+
+    </div>
+
+    <!--
+    Level 1
+    -->
+
+    <div class = "level" id = "one">
 
         <button type = "button" class = "tree_button" id = "if_else">
             If / Else
@@ -51,7 +76,9 @@
         <button type = "button" class = "tree_button" id = "print">
             Print
         </button>
+        
     </div>
+    
 
     <script src = "index.js"> </script>
 </body>
