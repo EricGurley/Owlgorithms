53dab88eea43e02f83c2d81adf8e52f8a37b6e00
= <ericanthonygurley@gmail.com>
2025-04-29 16:40:42 -0500

    code editor implemented kind of it's really bad

--- Main/print.php
+++ Main/print.php
@@ -4,9 +4,13 @@
 
 <body>
 
-    
+    <div class = "editor_container">
+        <div id = "editor">
+            // printf
+        </div>
+    </div>
 
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ace.js"></script>
 
-
-    <script src = "index.js"> </script>
+    <script src = "editor.js"> </script>
 </body>
